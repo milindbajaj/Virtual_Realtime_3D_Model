d8ee488135a6813f04fc807bf4203d00ab65feb9
milindbajaj <bajajvasu19@gmail.com>
2020-12-24 19:36:22 +0530

    commit by milind

---
+++ Assets/Scripts/LifecycleController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+#if UNITY_EDITOR
+// Set up touch input propagation while using Instant Preview in the editor.
+using Input = GoogleARCore.InstantPreviewInput;
+#endif
+
+public class LifecycleController : MonoBehaviour
+{
+    /// <summary>
+    /// True if the app is in the process of quitting due to an ARCore connection error, otherwise false.
+    /// </summary>
+    private bool m_IsQuitting;
+
+    void Start()
+    {
+#if DEVELOPMENT_BUILD
+        Debug.unityLogger.logEnabled = true;
+#else
+        Debug.unityLogger.logEnabled = false;
+#endif
+    }
+
+    /// <summary>
+    /// The Unity Awake() method.
+    /// </summary>
+    public void Awake()
+    {
+        // Enable ARCore to target 60fps camera capture frame rate on supported devices.
+        // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Example code to show toast on screen tap
+        //Touch touch;
+        //if (Input.touchCount != 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began) return;
+        //ShowAndroidToastMessage("Testing, 1... 2... 3...");
+
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to
+        // appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            ShowAndroidToastMessage(
+                "ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void DoQuit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private static void ShowAndroidToastMessage(string message)
+    {
+#if UNITY_ANDROID
+        using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            var unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+            if (unityActivity == null) return;
+            var toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                // Last parameter = length. Toast.LENGTH_LONG = 1
+                using (var toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText",
+                    unityActivity, message, 1))
+                {
+                    toastObject.Call("show");
+                }
+            }));
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/MarkerAnchorController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GoogleARCore;
+using UnityEngine;
+
+public class MarkerAnchorController : MonoBehaviour, IArObjectController
+{
+    /// <summary>
+    /// List of prefabs, sorted by the database IDs of the augmented image database file.
+    /// </summary>
+    public ArPrefab[] ArPrefabs;
+
+    /// <summary>
+    /// Only allows 1 gameobject instance per augmented images database index.
+    /// This array contains the instantiated gameobjects.
+    /// Array ID corresponds to database index.
+    /// Contains null if marker hasn't been found yet, or the gameobject instance
+    /// after marker has been detected (and is still being tracked).
+    /// </summary>
+    private GameObject[] m_InstantiatedPrefabs;
+    
+    /// <summary>
+    /// List filled by ARCore API containing all augmented images that have been updated.
+    /// </summary>
+    private readonly List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+
+
+    /// <summary>
+    /// The overlay containing the fit to scan user guide.
+    /// </summary>
+    //public GameObject FitToScanOverlay;
+
+    void Start()
+    {
+        // Only one instance per marker
+        m_InstantiatedPrefabs = new GameObject[ArPrefabs.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check that motion tracking is tracking.
+        if (Session.Status == SessionStatus.Tracking)
+        {
+            TrackAugmentedImages();
+        }
+    }
+
+
+    private void TrackAugmentedImages()
+    {
+        // Get updated augmented images for this frame.
+        Session.GetTrackables(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+
+        // Create visualizers and anchors for updated augmented images that are tracking and do not previously
+        // have a visualizer. Remove visualizers for stopped images.
+        foreach (var tempAugmentedImage in m_TempAugmentedImages)
+        {
+            // Sanity check to prevent out of bound index if a database index was returned
+            // where we don't have a prefab for.
+            if (m_InstantiatedPrefabs.Length <= tempAugmentedImage.DatabaseIndex) continue;
+
+
+            if (tempAugmentedImage.TrackingState == TrackingState.Tracking
+                && tempAugmentedImage.TrackingMethod == AugmentedImageTrackingMethod.FullTracking
+                && m_InstantiatedPrefabs[tempAugmentedImage.DatabaseIndex] == null)
+            {
+
+                // Prefab has not yet been instantiated in the scene
+                // -> Create a new anchor
+                var anchor = tempAugmentedImage.CreateAnchor(tempAugmentedImage.CenterPose);
+                // Instantiate the prefab corresponding to the augmented image.
+                var newArPrefab = Instantiate(ArPrefabs[tempAugmentedImage.DatabaseIndex], anchor.transform);
+                // Give our prefab instance the reference to its trackable so that it can adjust itself if needed.
+                newArPrefab.AttachedToTrackable = tempAugmentedImage;
+                // Make object child of the anchor
+                // Not done so in the AugmentedImages example, but in all others
+                // -> seems logical to do the same here, as planes are also Trackables,
+                // so they shouldn't be different.
+                newArPrefab.transform.parent = anchor.transform;
+                // Store the prefab instance at the position corresponding to the database index
+                m_InstantiatedPrefabs[tempAugmentedImage.DatabaseIndex] = newArPrefab.gameObject;
+                Debug.Log("MarkerAnchorController: Created new marker prefab instance, idx: " +
+                          tempAugmentedImage.DatabaseIndex);
+
+            }
+            else if ((tempAugmentedImage.TrackingState != TrackingState.Tracking ||
+                      tempAugmentedImage.TrackingMethod != AugmentedImageTrackingMethod.FullTracking) &&
+                     m_InstantiatedPrefabs[tempAugmentedImage.DatabaseIndex] != null)
+            {
+                DestroyPrefab(tempAugmentedImage.DatabaseIndex);
+            }
+        }
+    }
+
+
+
+    public bool ArePrefabsInstantiated()
+    {
+        return m_InstantiatedPrefabs.Any(instantiatedPrefab => instantiatedPrefab != null);
+    }
+
+    private void DestroyPrefab(int databaseIndex)
+    {
+        // Stopped tracking a previously found marker.
+        // Delete the anchor that was created in the Unity scene (its child is our own prefab instance)
+        Destroy(m_InstantiatedPrefabs[databaseIndex].transform.parent.gameObject);
+        // Now also delete the reference from our array so that we can create a new 
+        // instance if the marker is discovered again
+        m_InstantiatedPrefabs[databaseIndex] = null;
+        Debug.Log("MarkerAnchorController: Destroyed marker prefab instance, idx: " + databaseIndex);
+    }
+}
